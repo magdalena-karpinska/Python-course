475028008e47f704a3a8a657f01db309c36da8e9
Ginger Rose <gingerrose@MacBook-Pro-Ginger.local>
2024-02-07 17:02:51 +0100

    Creating classes practice

---
+++ python_classes_practice.py
@@ -0,0 +1,18 @@
+# Create a class called Player and print its variable level inside the definition.
+
+class Player:
+    level = 1
+    print(level)
+
+# Add a print statement that is inside the class definition, displaying the variable show.
+class TV_show:
+    show = "Spongebob Circleshirt"
+    channel = "Nickelodion"
+    print (show)
+
+#Add a variable called channel_num to the class and set its value to 56.
+class TV_show:
+    show = "Spongebob Circleshirt"
+    channel = "Nickelodion"
+    print(show)
+    channel_num = 56
